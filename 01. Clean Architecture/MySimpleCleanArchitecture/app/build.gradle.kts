eb52aa379bc4baa5a4ca8d54e6c0894e62ad7de5
Avrians <avriansyahb10@gmail.com>
2024-08-11 13:52:54 +0700

    [av][finish][Codelab Testing Use Case pada Clean Architecture]

--- 01. Clean Architecture/MySimpleCleanArchitecture/app/build.gradle.kts
+++ 01. Clean Architecture/MySimpleCleanArchitecture/app/build.gradle.kts
@@ -49,4 +49,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    testImplementation("org.mockito:mockito-core:4.4.0")
+    testImplementation("org.mockito:mockito-inline:4.4.0")
 }
