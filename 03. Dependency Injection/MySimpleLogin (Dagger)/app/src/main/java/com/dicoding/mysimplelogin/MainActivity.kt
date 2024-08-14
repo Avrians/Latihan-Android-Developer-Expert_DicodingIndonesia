154a0f64ccfd1418fc65d8f7ee40d4c4c7fa8e0f
Avrians <avriansyahb10@gmail.com>
2024-08-14 09:40:32 +0700

    [av][finish][Codelab Cek Singleton]

--- 03. Dependency Injection/MySimpleLogin (Dagger)/app/src/main/java/com/dicoding/mysimplelogin/MainActivity.kt
+++ 03. Dependency Injection/MySimpleLogin (Dagger)/app/src/main/java/com/dicoding/mysimplelogin/MainActivity.kt
@@ -11,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     @Inject
     lateinit var userRepository: UserRepository
+    @Inject
+    lateinit var userRepository2: UserRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         (application as MyApplication).appComponent.inject(this)
@@ -20,6 +22,9 @@ class MainActivity : AppCompatActivity() {
 
 //        val sesi = SessionManager(this)
 //        userRepository = UserRepository.getInstance(sesi)
+
+        userRepository.checkInstance()
+        userRepository2.checkInstance()
 
         if (userRepository.isUserLogin()) {
             moveToHomeActivity()
