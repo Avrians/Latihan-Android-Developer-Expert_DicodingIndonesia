f535ece8d71dc7a4c5faa84122acfa13c1de3929
avrians <avriansyahb10@gmail.com>
2024-09-21 07:20:35 +0700

    [add][submission2]

---
+++ 10. Submisison2/MyCookingSubmission2/core/src/main/java/com/avrians/core/di/NetworkModule.kt
@@ -0,0 +1,52 @@
+package com.avrians.core.di
+
+import com.avrians.core.BuildConfig
+import com.avrians.core.data.source.remote.network.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    private val hostname = "api.spoonacular.com"
+    private val certificatePinner = CertificatePinner.Builder()
+        .add(hostname, "sha256/PbTCXXUtf4SSIzyHDQIq2eO8kV4usvbnSOPc/CvgGGM=")
+        .add(hostname, "sha256/kIdp6NNEd8wsugYyyIYFsi1ylMCED3hZbSR8ZFsa/A4=")
+        .add(hostname, "sha256/mEflZT5enoR1FuXLgYYGqnVEoZvmf9c2bVBpiOjYQ0c=")
+        .build()
+
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        } else {
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
+            .build()
+    }
+
+    @Provides
+    fun provideApiService(client: OkHttpClient): ApiService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
+}
